ff381fadb9b6dee70e1c71ed809728d7b3897c7b
35bloob <you@example.com>
2026-05-14 08:56:23 +0200

    Switch to mirror source - No upload needed

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -1,12 +1,9 @@
-# AnyKernel3 - StealthStation v2.0
 do.devicecheck=1
 do.modules=0
 do.systemless=1
-do.cleanup=1
 device.name1=s5e9925
 device.name2=dm1s
 device.name3=dm1
-
-ui_print "StealthStation Kernel v2.0 Launched!";
+ui_print "StealthStation v2.0 - Mirror Build";
 dump_boot;
 write_boot;
