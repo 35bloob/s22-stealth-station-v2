9ab2f0f602ba205848230d67e286bb1cd64bcbcc
35bloob <35bloob@users.noreply.github.com>
2026-05-22 03:16:25 +0200

    Fix YAML indentation alignment

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -1,10 +1,39 @@
+### AnyKernel3 Ramdisk Mod Script
+## OpenSource MIT License
+
+#### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=StealthStation-V25 for Samsung Galaxy S22
 do.devicecheck=1
 do.modules=0
 do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
 device.name1=r0q
-device.name2=g0q
-device.name3=b0q
-device.name4=sm8450
-ui_print "StealthStation - AUTO SOLVED";
+device.name2=SM-S901E
+device.name3=SM-S901B
+device.name4=SM-S9010
+device.name5=SM-S901N
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; }
+
+# shell variables
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=1;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+#### AnyKernel methods (DO NOT CHANGE)
+# import patcher functions
+. tools/ak3-core.sh;
+
+#### AnyKernel install
 dump_boot;
+
+# (Minimal clean flash for modern Samsung layout - no legacy Nexus ramdisk edits)
+
 write_boot;
+#### end install
